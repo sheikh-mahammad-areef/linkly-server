1cd036b628ac54177f914cc8192b31c2ad420ec3
sheikhmahammadareef <sheikhmahammadareef@gmail.com>
2025-11-08 18:28:55 +0530

    feat: add HTTP status constants and improve error handling middleware

---
+++ src/config/http.config.ts
@@ -0,0 +1,27 @@
+// src/config/http.config.ts
+
+export const HTTP_STATUS = {
+  //   successful responses
+  OK: 200,
+  CREATED: 201,
+  ACCEPTED: 202,
+  NO_CONTENT: 204,
+
+  //   client error responses
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  UNPROCESSABLE_ENTITY: 422,
+  TOO_MANY_REQUESTS: 429,
+
+  //   server error responses
+  INTERNAL_SERVER_ERROR: 500,
+  NOT_IMPLEMENTED: 501,
+  BAD_GATEWAY: 502,
+  SERVICE_UNAVAILABLE: 503,
+  GATEWAY_TIMEOUT: 504,
+} as const;
+
+export type HttpStatusCode = (typeof HTTP_STATUS)[keyof typeof HTTP_STATUS];
--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,8 +1,17 @@
-import { Request, Response, NextFunction } from 'express';
+// src/middleware/error.middleware.ts
 
-export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
+import { HTTP_STATUS } from '../config/http.config';
+import { log } from 'console';
+
+export const errorHandler: ErrorRequestHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): any => {
   console.error(err.stack);
-  res.status(err.status || 500).json({
+  res.status(err.status || HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
     success: false,
     message: err.message || 'Internal Server Error',
   });
