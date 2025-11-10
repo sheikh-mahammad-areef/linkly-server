0662136c711c373d67deaaa24cfa88831468d22d
sheikhmahammadareef <sheikhmahammadareef@gmail.com>
2025-11-10 18:21:59 +0530

    feat: update development script to load environment variables from .env file; remove dotenv dependency

--- src/utils/env.utils.ts
+++ src/utils/env.utils.ts
@@ -1,5 +1,5 @@
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
 export const getEnv = (key: string, defaultValue: string = '') => {
   const value = process.env[key];
